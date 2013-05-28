15c94e57a1303fe5290870f35a2fc75a417b2739
Anupam Bagchi <anupam_bagchi@rocketmail.com>
2013-05-28 08:45:54 -0700

    Added lab8 run script.

---
+++ scripts/runlab_8.sh
@@ -0,0 +1,23 @@
+CLASSNAME=com.hadooptraining.lab8.LogProcessor
+
+echo Cleaning input and output directories...
+hadoop fs -rm -r output
+hadoop fs -rm -r input
+hadoop fs -mkdir input
+echo
+echo Copying input files to HDFS...
+hadoop fs -copyFromLocal -f data/access.log input
+echo
+echo Running Hadoop task...
+command="hadoop jar $DEV_HOME/target/heffalump-1.0.jar $CLASSNAME input output 2"
+echo $command
+$command
+echo ...Done
+echo
+echo Here are the files created in the output directory...
+hadoop fs -ls output
+echo The results are in two files since we specified two reducers.
+echo To see your results type:
+echo hadoop fs -cat output/part-r-00000
+echo hadoop fs -cat output/part-r-00001
+echo
