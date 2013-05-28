0996f4c15bfb9d89154ac63acefc3aa6d07d78f7
Anupam Bagchi <anupam_bagchi@rocketmail.com>
2013-05-28 10:45:39 -0700

    Some formatting changes.

--- src/main/java/com/hadooptraining/lab8/LogProcessorMap.java
+++ src/main/java/com/hadooptraining/lab8/LogProcessorMap.java
@@ -6,11 +6,11 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class LogProcessorMap extends Mapper<Object, LogWritable, Text, IntWritable > {
+public class LogProcessorMap extends Mapper<Object, LogWritable, Text, IntWritable> {
+
     public void map(Object key, LogWritable value, Context context)
             throws IOException, InterruptedException {
 
-        // make bytes longWritable and output two value types...
-        context.write(value.getUserIP(),value.getResponseSize());
+        context.write(value.getUserIP(),value.getResponseSize());  // make bytes longWritable and output two value types
     }
 }
